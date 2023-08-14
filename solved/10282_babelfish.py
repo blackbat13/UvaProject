975d25976b74487359abb7f275337c08851750ed
blackbat13 <blackbat13@gmail.com>
2023-08-15 00:09:05 +0200

    Create 10282_babelfish.py

---
+++ solved/10282_babelfish.py
@@ -0,0 +1,21 @@
+dictionary = dict()
+
+while True:
+    line = input()
+    if len(line) <= 1:
+        break
+
+    translation, word = line.split(" ")
+    dictionary[word] = translation
+
+
+while True:
+    try:
+        word = input()
+    except EOFError:
+        break
+
+    if word not in dictionary:
+        print("eh")
+    else:
+        print(dictionary[word])
