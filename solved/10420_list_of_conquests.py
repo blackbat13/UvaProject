524768f307c2e4c74bf4a39a8ffa546f8a87efe9
blackbat13 <blackbat13@gmail.com>
2023-08-15 00:07:06 +0200

    Create 10420_list_of_conquests.py

---
+++ solved/10420_list_of_conquests.py
@@ -0,0 +1,13 @@
+counters = dict()
+n = int(input())
+
+for _ in range(n):
+    country = input().split(" ")[0]
+
+    if country in counters:
+        counters[country] += 1
+    else:
+        counters[country] = 1
+
+for country in sorted(counters):
+    print(f"{country} {counters[country]}")
