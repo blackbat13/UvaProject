8a5e2142c7e1a6b76811e62bf5fb77c2f9d9ad9f
blackbat13 <blackbat13@gmail.com>
2023-08-15 00:13:20 +0200

    Update 11854_egypt.cpp

--- solved/11854_egypt.cpp
+++ solved/11854_egypt.cpp
@@ -1,20 +1,23 @@
-//
-// Created by black on 24.03.2017.
-//
-
 #include <cstdio>
 #include <algorithm>
+
 using namespace std;
 
-int main() {
+int main()
+{
     int tab[3];
-    while (scanf("%d %d %d", &tab[0], &tab[1], &tab[2]) && (tab[0] != 0 || tab[1] != 0 || tab[2] != 0)) {
-        sort(tab, tab+3);
-        if(tab[0]*tab[0] + tab[1]*tab[1] == tab[2]*tab[2]) {
+    while (scanf("%d %d %d", &tab[0], &tab[1], &tab[2]) && (tab[0] != 0 || tab[1] != 0 || tab[2] != 0))
+    {
+        sort(tab, tab + 3);
+        if (tab[0] * tab[0] + tab[1] * tab[1] == tab[2] * tab[2])
+        {
             printf("right\n");
-        } else {
+        }
+        else
+        {
             printf("wrong\n");
         }
     }
+    
     return 0;
 }
