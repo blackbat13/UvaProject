d480acf56150cd508731fc0b252bceee9cd5b62b
blackbat13 <blackbat13@gmail.com>
2023-08-15 00:05:24 +0200

    Create 12854_automated_checking_machine.py

---
+++ solved/12854_automated_checking_machine.py
@@ -0,0 +1,16 @@
+while True:
+    try:
+        tab1 = list(map(int, input().split()))
+        tab2 = list(map(int, input().split()))
+    except EOFError:
+        break
+
+    result = True
+    for el1, el2 in zip(tab1, tab2):
+        if el1 == el2:
+            result = False
+
+    if result:
+        print("Y")
+    else:
+        print("N")
